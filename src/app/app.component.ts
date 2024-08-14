35abf133b95cac65025a8efba238155255b8fae1
Vipul Aggarwal <97421638+VipulAggarwalCtr@users.noreply.github.com>
2024-08-14 04:40:50 +0000

    added a high level logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-app';
+
+  someHighLevelFunc(arg1: number, arg2: number) {
+    const sum = arg1 + arg2;
+    const diff = Math.abs(arg1 - arg2);
+    const product = arg1 * arg2;
+    const division = arg1 / arg2;
+
+    return { sum, product, diff, division };
+  }
 }
